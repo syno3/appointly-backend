7cc8f2c4afba74f2627db10c4a06435033ad85b4
Festus murimi <sinotechxvista@gmail.com>
2022-08-24 18:22:20 +0300

    added crud functionality for meetings

--- controllers/get.js
+++ controllers/get.js
@@ -40,3 +40,78 @@ export const signUp = async (req, res) => {
 }
 
 //create a new meeting
+export const createMeeting = async (req, res) => {
+    const title = req.body.title;
+    const description = req.body.description;
+    const payment = req.body.payment;
+    const capacity = req.body.capacity;
+    const type = req.body.type;
+    const link = req.body.link;
+    const status = req.body.status;
+    const date_start = req.body.date_start;
+    const time = req.body.time;
+    const duration = req.body.duration;
+    const cover = req.body.cover;
+
+    const owner = req.body.owner;
+
+    const { data, error } = await supabase
+    .from('Meetings')
+    .insert([
+        { title: title, description: description, payment_Amount: payment, capacity: capacity, type: type, link: link, status: status, date_start: date_start, time: time, duration: duration, coverPhoto: cover, owner: owner },
+  ])
+
+    if(error){
+        res.send(error).status(400);
+    }
+
+    res.send(data).status(200);
+}
+
+//get all meetings that user created
+export const getMeetings = async (req, res) => {
+    const owner = req.body.owner;
+    const {data: meetings, error} = await supabase
+    .from('Meetings')
+    .select("*")
+    .eq('owner', owner)
+
+    if(error){
+        res.send(error).status(400);
+    }
+    res.send(meetings).status(200);
+}
+
+//get a specific meeting
+export const getMeeting = async (req, res) => {
+    const id = req.query.id; //id of meeting in query
+    const owner = req.body.owner; //owner of meeting in body
+    const {data: meeting, error} = await supabase
+    .from('Meetings')
+    .select("*")
+    .eq('owner', owner)
+    .eq('id', id)
+
+    if(error){
+        res.send(error).status(400);
+    }
+    res.send(meeting).status(200);
+}
+
+//delete a meeting
+export const deleteMeeting = async (req, res) => {
+    const id = req.query.id; //id of meeting in query
+    const owner = req.body.owner; //owner of meeting in body
+    const {data: meeting, error} = await supabase
+    .from('Meetings')
+    .delete()
+    .eq('owner', owner)
+    .eq('id', id)
+
+    if(error){
+        res.send(error).status(400);
+    }
+    res.send(meeting).status(200);
+}
+
+
--- routes/api.js
+++ routes/api.js
@@ -2,16 +2,30 @@ import express from "express";
 import {
   testUser,
   login,
-  signUp
+  signUp,
+  getMeetings,
+  getMeeting,
+  createMeeting,
+  deleteMeeting
 } from '../controllers/get.js';
 
 const router = express.Router();
 
-//test route
+//get routes
 router.get("/", testUser);
+router.get("/getMeetings", getMeetings);
+router.get("/getMeeting", getMeeting);
 
+
+//post routes
 router.post("/login", login);
 router.post("/signup", signUp);
+router.post("/createMeeting", createMeeting);
+
+
+
+//delete routes
+router.delete("/deleteMeeting", deleteMeeting)
 
 
 export default router;
