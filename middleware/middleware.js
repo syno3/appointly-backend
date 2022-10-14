b8edf717526504433adb2e4bc0da78cb45e999a1
Festus murimi <sinotechxvista@gmail.com>
2022-10-14 10:51:48 +0300

    added get reviews and formatted code

--- middleware/middleware.js
+++ middleware/middleware.js
@@ -1,7 +1,7 @@
 import dotenv from "dotenv";
 import axios from "axios";
 import jwt from "jsonwebtoken";
-import { supabase } from "../utils/supabaseClient.js"
+import { supabase } from "../utils/supabaseClient.js";
 
 dotenv.config();
 
@@ -38,18 +38,19 @@ export const MpesaToken = async (req, res, next) => {
   }
 };
 
-
 // autheticate supabase acesstoken
 export const authenticateToken = async (req, res, next) => {
   const authHeader = req.headers["authorization"];
   const token = authHeader && authHeader.split(" ")[1];
   if (token == null) return res.sendStatus(401);
 
-  try{
-    const { data: { user } } = await supabase.auth.getUser(token);
+  try {
+    const {
+      data: { user },
+    } = await supabase.auth.getUser(token);
     req.user = user;
     next();
-  }catch(err){
+  } catch (err) {
     return res.sendStatus(403);
   }
-}
+};
