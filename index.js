e411b29566f5e120909e6827d934a5272d8112d8
Festus murimi <sinotechxvista@gmail.com>
2022-11-21 13:36:17 +0300

    added more data to cors

--- index.js
+++ index.js
@@ -10,6 +10,9 @@ app.use(bodyparser.json({ limit: "50mb", extended: true }));
 app.use(bodyparser.urlencoded({ limit: "50mb", extended: true }));
 app.use(cors({
   origin: "*",
+  methods : "GET,HEAD,PUT,PATCH,POST,DELETE",
+  preflightContinue: false,
+  optionsSuccessStatus: 204
 }));
 
 const options = {
