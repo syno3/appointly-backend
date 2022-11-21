087f9351557e5f4a944a858b16be74659e29220a
Festus murimi <sinotechxvista@gmail.com>
2022-11-21 13:56:06 +0300

    added /

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(bodyparser.json({ limit: "50mb", extended: true }));
 app.use(bodyparser.urlencoded({ limit: "50mb", extended: true }));
 app.use(cors({
-  origin: "https://www.appointly.co",
+  origin: "https://www.appointly.co/",
   methods : "GET,HEAD,PUT,PATCH,POST,DELETE",
   preflightContinue: false,
   optionsSuccessStatus: 204
